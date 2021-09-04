94a502c89923612bb769c3567b84fb6b75a9e719
Allen Lin <azflin@users.noreply.github.com>
2021-09-03 22:14:23 -0400

    first draft

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -16,16 +16,16 @@ contract Marketplace {
         uint price;
     }
 
-    address public weth;
+    IERC20 public weth;
     mapping (address => mapping(uint => Offer)) public offers;
     mapping (address => mapping(uint => Bid)) public bids;
 
     event NewBid(address indexed bidder, uint price, address indexed erc721, uint tokenId);
     event NewOffer(address indexed seller, uint price, address indexed erc721, uint tokenId);
-    event Sale(address indexed bidder, address indexed seller, uint price, address indexed erc721, uint tokenId);
+    event Sale(address indexed buyer, address indexed seller, uint price, address indexed erc721, uint tokenId);
 
-    constructor(address _weth) public {
-        weth = _weth;
+    constructor(address _weth) {
+        weth = IERC20(_weth);
     }
 
     function makeOffer(address _erc721, uint _tokenId, uint _price) public {
@@ -33,6 +33,7 @@ contract Marketplace {
         require(_price > 0, "Cannot offer 0.");
         Offer memory offer = Offer(payable(msg.sender), _price);
         offers[_erc721][_tokenId] = offer;
+        emit NewOffer(msg.sender, _price, _erc721, _tokenId);
     }
 
     function takeOffer(address _erc721, uint _tokenId) public payable {
@@ -44,13 +45,55 @@ contract Marketplace {
         (bool success, ) = offer.seller.call{value: msg.value}("");
         require(success, "Transfer was unsuccessful.");
         IERC721(_erc721).safeTransferFrom(offer.seller, payable(msg.sender), _tokenId);
+        emit Sale(msg.sender, offer.seller, offer.price, _erc721, _tokenId);
+        // Clear out offer and bid
+        offer.seller = payable(address(0));
+        offer.price = 0;
+        Bid storage bid = bids[_erc721][_tokenId];
+        bid.bidder = payable(address(0));
+        bid.price = 0;
     }
 
     function makeBid(address _erc721, uint _tokenId, uint _bid) public {
-        require(IERC721(_erc721).ownerOf(_tokenId) != msg.sender, "You cannot bid on your own NFT.");
+        require(IERC721(_erc721).ownerOf(_tokenId) != msg.sender,
+            "You cannot bid on your own NFT.");
+        require(weth.allowance(msg.sender, address(this)) >= _bid,
+            "You did not give allowance for marketplace to spend your WETH.");
+        require(_bid > 0, "Bid must be > 0.");
+        Bid storage bid = bids[_erc721][_tokenId];
+        bid.bidder = msg.sender;
+        bid.price = _bid;
+        emit NewBid(msg.sender, _bid, _erc721, _tokenId);
     }
 
-    function takeBid(address _erc721, uint _tokenId) public payable {
+    function takeBid(address _erc721, uint _tokenId) public {
+        require(IERC721(_erc721).ownerOf(_tokenId) == msg.sender, "You do not own this NFT.");
+        Bid storage bid = bids[_erc721][_tokenId];
+        require(bid.price > 0, "There is no bid.");
+        IERC721(_erc721).safeTransferFrom(msg.sender, bid.bidder, _tokenId);
+        weth.transferFrom(bid.bidder, msg.sender, bid.price);
+        emit Sale(bid.bidder, msg.sender, bid.price, _erc721, _tokenId);
+        // Clear out bid and offer
+        bid.bidder = payable(address(0));
+        bid.price = 0;
+        Offer storage offer = offers[_erc721][_tokenId];
+        offer.seller = payable(address(0));
+        offer.price = 0;
+    }
 
+    function removeOffer(address _erc721, uint _tokenId) public {
+        require(IERC721(_erc721).ownerOf(_tokenId) == msg.sender, "You do not own this NFT.");
+        Offer storage offer = offers[_erc721][_tokenId];
+        offer.price = 0;
+        offer.seller = payable(address(0));
+        emit NewOffer(address(0), 0, _erc721, _tokenId);
+    }
+
+    function removeBid(address _erc721, uint _tokenId) public {
+        Bid storage bid = bids[_erc721][_tokenId];
+        require(bid.bidder == msg.sender, "You are not the bidder.");
+        bid.price = 0;
+        bid.bidder = payable(address(0));
+        emit NewBid(address(0), 0, _erc721, _tokenId);
     }
 }
--- test/marketplace.js
+++ test/marketplace.js
@@ -21,8 +21,14 @@ describe("Marketplace contract", function () {
     [this.account1, this.account2, this.account3] = await ethers.getSigners();
     this.marketplace = await ethers.getContractAt("Marketplace", marketplace.address);
     this.dummyNFT = await ethers.getContractAt("DummyNFT", dummyNFT.address);
+    // Give account1 NFT id 1 and give account 2 NFT id 2
     this.dummyNFT.mintDNFT(this.account1.address, '');
     this.dummyNFT.mintDNFT(this.account2.address, '');
+    // Approve marketplace to spend account1's WETH and account 3's WETH
+    let connectedWeth = this.weth.connect(this.account1);
+    await connectedWeth.approve(marketplace.address, ethers.constants.MaxUint256);
+    connectedWeth = this.weth.connect(this.account3);
+    await connectedWeth.approve(marketplace.address, ethers.constants.MaxUint256);
   });
 
   it("should not make offer for NFT you don't own", async function () {
@@ -39,7 +45,9 @@ describe("Marketplace contract", function () {
 
   it("should allow you to offer an NFT", async function () {
     this.marketplace = this.marketplace.connect(this.account1);
-    await this.marketplace.makeOffer(this.dummyNFT.address, 1, ethers.utils.parseEther("1"));
+    await expect(this.marketplace.makeOffer(this.dummyNFT.address, 1, ethers.utils.parseEther("1")))
+      .to.emit(this.marketplace, 'NewOffer')
+      .withArgs(this.account1.address, ethers.utils.parseEther("1"), this.dummyNFT.address, 1);
     let offer = await this.marketplace.offers(this.dummyNFT.address, 1);
     expect(offer.seller).to.equal(this.account1.address);
     expect(offer.price).to.equal(ethers.utils.parseEther("1"));
@@ -93,17 +101,43 @@ describe("Marketplace contract", function () {
   });
 
   it("should takeOffer", async function () {
+    // Account 1 makes an offer of NFT 1 for 1 ether
     this.marketplace = this.marketplace.connect(this.account1);
     await this.marketplace.makeOffer(this.dummyNFT.address, 1, ethers.utils.parseEther("1"));
+
+    // Account 3 makes a bid on NFT 1 for 0.5 ether
+    this.marketplace = this.marketplace.connect(this.account3);
+    await this.marketplace.makeBid(this.dummyNFT.address, 1, ethers.utils.parseEther("0.5"));
+    let bid = await this.marketplace.bids(this.dummyNFT.address, 1);
+    expect(bid.bidder).to.be.equal(this.account3.address);
+    expect(bid.price).to.be.equal(ethers.utils.parseEther("0.5"));
+
+    // Account 1 has to approve the marketplace to transfer the NFT
     this.dummyNFT = this.dummyNFT.connect(this.account1);
     await this.dummyNFT.setApprovalForAll(this.marketplace.address, true);
+
+    // Account 2 takes this offer
     this.marketplace = this.marketplace.connect(this.account2);
     let oldBalance = await ethers.provider.getBalance(this.account2.address);
-    await this.marketplace.takeOffer(this.dummyNFT.address, 1, {value: ethers.utils.parseEther("1")});
+    // Check to see Sale event emitted
+    await expect(this.marketplace.takeOffer(this.dummyNFT.address, 1, {value: ethers.utils.parseEther("1")}))
+      .to.emit(this.marketplace, 'Sale')
+      .withArgs(this.account2.address, this.account1.address,
+        ethers.utils.parseEther("1"), this.dummyNFT.address, 1);
+    // Check the new ownership of NFT 1 is Account 2
     expect(await this.dummyNFT.ownerOf(1)).to.be.equal(this.account2.address);
+    // Check balance of account 2 is less.
     let newBalance = await ethers.provider.getBalance(this.account2.address);
     // Difference should be slightly greater because of gas
     expect(oldBalance.sub(newBalance)).to.be.gt(ethers.utils.parseEther("1"));
+    // There should be no more offer for that NFT
+    let offer = await this.marketplace.offers(this.dummyNFT.address, 1);
+    expect(offer.seller).to.be.equal(ethers.constants.AddressZero);
+    expect(offer.price).to.be.equal(0);
+    // There should also be no more bid on that NFT
+    bid = await this.marketplace.bids(this.dummyNFT.address, 1);
+    expect(bid.bidder).to.be.equal(ethers.constants.AddressZero);
+    expect(bid.price).to.be.equal(0);
   });
 
   it("should revert makeBid if bidding on your own NFT", async function () {
@@ -111,5 +145,124 @@ describe("Marketplace contract", function () {
     await expect(
       this.marketplace.makeBid(this.dummyNFT.address, 1, ethers.utils.parseEther("1"))
     ).to.be.revertedWith("You cannot bid on your own NFT.");
+  });
+
+  it("should revert makeBid if bid is 0", async function () {
+    this.marketplace = this.marketplace.connect(this.account1);
+    await expect(
+      this.marketplace.makeBid(this.dummyNFT.address, 2, 0)
+    ).to.be.revertedWith("Bid must be > 0.");
+  });
+
+  it("should revert makeBid if you didnt give the contract allowance to spend your ETH", async function () {
+    this.weth = this.weth.connect(this.account1);
+    await this.weth.approve(this.marketplace.address, 0);
+    this.marketplace = this.marketplace.connect(this.account1);
+    await expect(
+      this.marketplace.makeBid(
+        this.dummyNFT.address,
+        2,
+        ethers.utils.parseEther("1")
+      )
+    ).to.be.revertedWith(
+      "You did not give allowance for marketplace to spend your WETH.");
+  });
+
+  it("should makeBid", async function () {
+    this.marketplace = this.marketplace.connect(this.account1);
+    await expect(this.marketplace.makeBid(this.dummyNFT.address, 2, ethers.utils.parseEther("1")))
+      .to.emit(this.marketplace, 'NewBid')
+      .withArgs(this.account1.address, ethers.utils.parseEther("1"), this.dummyNFT.address, 2);
+    const bid = await this.marketplace.bids(this.dummyNFT.address, 2);
+    expect(bid.bidder).to.be.equal(this.account1.address);
+    expect(bid.price).to.be.equal(ethers.utils.parseEther("1"));
+  });
+
+  it("should revert takeBid if you don't own NFT", async function () {
+    this.marketplace = this.marketplace.connect(this.account1);
+    await this.marketplace.makeBid(this.dummyNFT.address, 2, ethers.utils.parseEther("1"));
+    this.marketplace = this.marketplace.connect(this.account3);
+    await expect(this.marketplace.takeBid(this.dummyNFT.address, 2)).to.be.revertedWith(
+      "You do not own this NFT.");
+  });
+
+  it("should revert takeBid if there is no bid", async function () {
+    this.marketplace = this.marketplace.connect(this.account2);
+    await expect(this.marketplace.takeBid(this.dummyNFT.address, 2)).to.be.revertedWith(
+      "There is no bid.");
+  });
+
+  it("should takeBid", async function () {
+    // Account 1 makes a bid of NFT 2 for 1 ether
+    this.marketplace = this.marketplace.connect(this.account1);
+    await this.marketplace.makeBid(this.dummyNFT.address, 2, ethers.utils.parseEther("1"));
+
+    // Account 2 makes an offer of NFT 2 for 2 ether
+    this.marketplace = this.marketplace.connect(this.account2);
+    await this.marketplace.makeOffer(this.dummyNFT.address, 2, ethers.utils.parseEther("2"));
+    let offer = await this.marketplace.offers(this.dummyNFT.address, 2);
+    expect(offer.seller).to.be.equal(this.account2.address);
+    expect(offer.price).to.be.equal(ethers.utils.parseEther("2"));
+
+    // Account 2 has to approve the marketplace to transfer the NFT
+    this.dummyNFT = this.dummyNFT.connect(this.account2);
+    await this.dummyNFT.setApprovalForAll(this.marketplace.address, true);
+
+    // Account 2 takes this bid
+    // Check to see Sale event emitted
+    await expect(this.marketplace.takeBid(this.dummyNFT.address, 2))
+      .to.emit(this.marketplace, 'Sale')
+      .withArgs(this.account1.address, this.account2.address,
+        ethers.utils.parseEther("1"), this.dummyNFT.address, 2);
+    // Check the new ownership of NFT 2 is Account 1
+    expect(await this.dummyNFT.ownerOf(2)).to.be.equal(this.account1.address);
+    // Check balance of account 2 is 1 WETH.
+    expect(await weth.balanceOf(this.account2.address)).to.be.equal(ethers.utils.parseEther("1"));
+    // Check balance of account 1 is 1 WETH less.
+    expect(await weth.balanceOf(this.account1.address)).to.be.equal(ethers.utils.parseEther("999"));
+    // There should be no more offer for that NFT
+    offer = await this.marketplace.offers(this.dummyNFT.address, 2);
+    expect(offer.seller).to.be.equal(ethers.constants.AddressZero);
+    expect(offer.price).to.be.equal(0);
+    // There should also be no more bid on that NFT
+    bid = await this.marketplace.bids(this.dummyNFT.address, 2);
+    expect(bid.bidder).to.be.equal(ethers.constants.AddressZero);
+    expect(bid.price).to.be.equal(0);
+  });
+
+  it("should revert removeOffer if you don't own the NFT", async function () {
+    this.marketplace = this.marketplace.connect(this.account1);
+    await expect(this.marketplace.removeOffer(this.dummyNFT.address, 2)).to.be.revertedWith(
+      "You do not own this NFT.");
+  });
+
+  it("should removeOffer", async function () {
+    this.marketplace = this.marketplace.connect(this.account1);
+    await this.marketplace.makeOffer(this.dummyNFT.address, 1, ethers.utils.parseEther("1"));
+    await expect(this.marketplace.removeOffer(this.dummyNFT.address, 1))
+      .to.emit(this.marketplace, 'NewOffer')
+      .withArgs(ethers.constants.AddressZero, 0, this.dummyNFT.address, 1);
+    let offer = await this.marketplace.offers(this.dummyNFT.address, 1);
+    expect(offer.seller).to.be.equal(ethers.constants.AddressZero);
+    expect(offer.price).to.be.equal(0);
+  });
+
+  it("should revert removeBid if you aren't the bidder", async function () {
+    this.marketplace = this.marketplace.connect(this.account1);
+    await this.marketplace.makeBid(this.dummyNFT.address, 2, ethers.utils.parseEther("1"));
+    this.marketplace = this.marketplace.connect(this.account3);
+    await expect(this.marketplace.removeBid(this.dummyNFT.address, 2)).to.be.revertedWith(
+      "You are not the bidder.");
+  });
+
+  it("should removeBid", async function () {
+    this.marketplace = this.marketplace.connect(this.account1);
+    await this.marketplace.makeBid(this.dummyNFT.address, 2, ethers.utils.parseEther("1"));
+    await expect(this.marketplace.removeBid(this.dummyNFT.address, 2))
+      .to.emit(this.marketplace, 'NewBid')
+      .withArgs(ethers.constants.AddressZero, 0, this.dummyNFT.address, 2);
+    let bid = await this.marketplace.bids(this.dummyNFT.address, 2);
+    expect(bid.bidder).to.be.equal(ethers.constants.AddressZero);
+    expect(bid.price).to.be.equal(0);
   });
 });
